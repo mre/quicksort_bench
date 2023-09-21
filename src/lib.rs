3d74b3b3941bbccc1745338597426947a394a7c6
Matthias <matthias-endler@gmx.net>
2023-09-21 16:12:53 +0200

    tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,33 @@
 use rayon::prelude::*;
 
+/// Mutable, in-place quicksort implementation
+pub fn quicksort_mut<T: Ord>(mut arr: Vec<T>) -> Vec<T> {
+    if arr.len() <= 1 {
+        return arr;
+    }
+
+    let pivot = arr.remove(0);
+    let mut left = vec![];
+    let mut right = vec![];
+
+    for item in arr {
+        if item <= pivot {
+            left.push(item);
+        } else {
+            right.push(item);
+        }
+    }
+
+    let mut sorted_left = quicksort_mut(left);
+    let mut sorted_right = quicksort_mut(right);
+
+    sorted_left.push(pivot);
+    sorted_left.append(&mut sorted_right);
+
+    sorted_left
+}
+
+/// Immutable quicksort implementation
 pub fn quicksort<T: Ord + Clone>(array: &[T]) -> Vec<T> {
     if array.len() <= 1 {
         return array.to_vec();
@@ -12,6 +40,7 @@ pub fn quicksort<T: Ord + Clone>(array: &[T]) -> Vec<T> {
     [quicksort(&lower), vec![pivot.clone()], quicksort(&higher)].concat()
 }
 
+/// Uses rayon to parallelize the quicksort algorithm
 pub fn quicksort_par<T: Ord + Clone + Sync + Send>(array: &[T]) -> Vec<T> {
     if array.len() <= 1 {
         return array.to_vec();
@@ -37,28 +66,75 @@ pub fn quicksort_par<T: Ord + Clone + Sync + Send>(array: &[T]) -> Vec<T> {
     .concat()
 }
 
-pub fn quicksort_mut<T: Ord>(mut arr: Vec<T>) -> Vec<T> {
-    if arr.len() <= 1 {
-        return arr;
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    pub fn is_sorted<T: Ord + std::fmt::Debug>(arr: &[T]) -> bool {
+        arr.windows(2).all(|win| win[0] <= win[1])
     }
 
-    let pivot = arr.remove(0);
-    let mut left = vec![];
-    let mut right = vec![];
+    fn get_test_vecs() -> Vec<Vec<u64>> {
+        vec![
+            vec![],
+            vec![1],
+            vec![1, 2],
+            vec![2, 1],
+            vec![1, 2, 3],
+            vec![2, 1, 3],
+            vec![3, 1, 2],
+            vec![8, 5, 2, 6, 9, 3],
+            vec![2, 3, 5, 6, 8, 9],
+            vec![9, 8, 6, 5, 3, 2],
+            vec![8, 4, 7, 3, 6, 2, 5, 1],
+            vec![8, 1, 7, 2, 6, 3, 5, 4],
+            vec![8, 1, 7, 2, 6, 3, 5, 4],
+            vec![16, 14, 1, 1, 7, 18, 7, 6, 8, 18, 5],
+            vec![19, 18, 14, 15, 3, 9, 8, 2, 2, 20, 11],
+            vec![
+                2, 3, 8, 7, 23, 26, 19, 29, 23, 32, 20, 18, 11, 11, 24, 13, 17,
+            ],
+            vec![0, 3, 7, 6],
+            vec![6, 4, 4, 5, 11, 10, 10],
+            vec![15, 13, 17, 1, 1, 19, 3, 19, 0, 11],
+            vec![
+                19, 19, 21, 21, 22, 25, 19, 14, 23, 25, 14, 10, 8, 4, 28, 12, 2, 33,
+            ],
+            vec![8, 1, 0, 5, 3],
+            vec![
+                27, 14, 22, 10, 8, 23, 7, 32, 28, 31, 9, 19, 30, 28, 21, 20, 13,
+            ],
+            vec![2, 1, 4, 17, 5, 17, 8, 2, 13, 13],
+        ]
+    }
 
-    for item in arr {
-        if item <= pivot {
-            left.push(item);
-        } else {
-            right.push(item);
+    #[test]
+    fn test_quicksort_mut() {
+        let tests = get_test_vecs();
+
+        for test in tests {
+            let result = quicksort_mut(test);
+            assert!(is_sorted(&result));
         }
     }
 
-    let mut sorted_left = quicksort_mut(left);
-    let mut sorted_right = quicksort_mut(right);
+    #[test]
+    fn test_quicksort() {
+        let tests = get_test_vecs();
 
-    sorted_left.push(pivot);
-    sorted_left.append(&mut sorted_right);
+        for test in tests {
+            let result = quicksort(&test);
+            assert!(is_sorted(&result));
+        }
+    }
 
-    sorted_left
+    #[test]
+    fn test_quicksort_par() {
+        let tests = get_test_vecs();
+
+        for test in tests {
+            let result = quicksort_par(&test);
+            assert!(is_sorted(&result));
+        }
+    }
 }
